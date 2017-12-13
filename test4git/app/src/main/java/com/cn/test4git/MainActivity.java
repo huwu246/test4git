26a8aeaa0a8e5eaad1ed85600a1a5f78060e4455
huwu246 <huwu246@gmail.com>
2017-12-14 01:24:40 +0800

    add autoLayout
    Signed-off-by: huwu246 <huwu246@gmail.com>

---
+++ test4git/app/src/main/java/com/cn/test4git/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cn.test4git;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zhy.autolayout.AutoLayoutActivity;
+
+public class MainActivity extends AutoLayoutActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
